697626325fdd76b0753610a6cb7577e2554a1229
giannis <gkorod@hua.gr>
2023-10-17 18:41:39 +0300

    latest version of instancemanager

--- instancemanager/consume.py
+++ instancemanager/consume.py
@@ -6,6 +6,8 @@ import json
 import pika
 import requests
 from requests.auth import HTTPBasicAuth
+import subprocess
+from subprocess import PIPE
 
 # from MessageThread import CustomThread
 consumer_list = []
@@ -30,18 +32,26 @@ def callback(ch, method, properties, body):
 
     my_json = body.decode('utf8')
     data = json.loads(my_json)
-    json_format = json.dumps(data, indent=4, sort_keys=False)
-    logging.info(" message consumed ")
     ch.basic_ack(delivery_tag=method.delivery_tag)
-    return json_format
+    namespace = data.get("namespace")
+    pod = data.get("pod")
+    x = subprocess.Popen(["kubectl delete -n "+namespace+" pod "+pod],shell=True,stdout=PIPE,stderr=PIPE)
+    stdout, stderr = x.communicate()
+    if not "not found" in stderr.decode("utf-8"):
+    	message = "pod "+ pod+ " deleted"
+    else:
+       message= "looking for pods"
+    print(message)
+    return message
 
 
 def consume_message(topic):
     channel = consume_connection.channel()
-    channel.basic_qos(prefetch_count=0)
+    channel.basic_qos(prefetch_count=10)
     result = channel.basic_consume(queue=topic,
                                    auto_ack=False,
                                    on_message_callback=callback)
-    logging.info(' [*] Waiting for messages.')
+    print(' [*] Waiting for messages.')
     channel.start_consuming()
     return result
+
--- instancemanager/main.py
+++ instancemanager/main.py
@@ -1,65 +1,15 @@
-import requests
-
-import consume
+from consume import consume_message
 import os
+import subprocess
+import logging
+import json
 
-
-
-
-def find_vhosts():
-    # RabbitMQ credentials
-    user = os.getenv("RABBITMQ_USERNAME", "name")
-    rabbit_ip = os.getenv("POD_IP", "ip")
-    password = os.getenv("RABBITMQ_PASSWORD", "password")
-
-    rabbitmq_api_url = 'http://'+rabbit_ip+':15672/api/vhosts'  # Replace with your RabbitMQ Management API URL
-    # Basic authentication header for RabbitMQ Management API
-    auth_header = (user, password)
-
-    # Make a GET request to retrieve vhosts
-    response = requests.get(rabbitmq_api_url, auth=auth_header)
-
-    vhosts = response.json()
-    print("Available vhosts:")
-    for vhost in vhosts:
-        print(vhost['name'])
-    return vhosts
-
-
-import os
-import requests
-
-
-def find_queues():
-    # RabbitMQ credentials
-    user = os.getenv("RABBITMQ_USERNAME", "name")
-    rabbit_ip = os.getenv("POD_IP", "ip")
-    password = os.getenv("RABBITMQ_PASSWORD", "password")
-
-    rabbitmq_api_url = f'http://'+rabbit_ip+':15672/api/queues'  # RabbitMQ Management API URL for queues
-    # Basic authentication header for RabbitMQ Management API
-    auth_header = (user, password)
-
-    # Make a GET request to retrieve queues
-    response = requests.get(rabbitmq_api_url, auth=auth_header)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        queues = response.json()
-        print("Available queues:")
-        for queue in queues:
-            print(queue['name'])
-        return queues
-    else:
-        print(f"Failed to retrieve queues. Status code: {response.status_code}")
-        return None
-
-
-
-
-vhosts = find_vhosts()
-queues = find_queues()
+logging.getLogger().setLevel(logging.INFO)
 rabbit_input_topic = os.getenv("INPUT_TOPIC", "topic-2")
-json_data = consume.consume_message(rabbit_input_topic)
-print(json_data)
-
+json_data = consume_message(rabbit_input_topic)
+namespace = json_data.get("namespace")
+pod = json.get("pod")
+command = "kubectl delete -n "+namespace+" pod "+pod
+print(command)
+subprocess.call(["kubectl delete -n "+namespace+" pod "+pod], shell=True)
+logging.info("pod "+ pod+ " deleted")
