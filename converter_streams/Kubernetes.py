791af4c352059f84ba153b9e7d123b4dd0255681
eddoog <codewithalfredo@gmail.com>
2025-03-31 23:11:55 +0700

    Change generation of scaled object and use static dir with timestamp instead of creating temp dir

--- converter_streams/Kubernetes.py
+++ converter_streams/Kubernetes.py
@@ -10,14 +10,18 @@ import socket
 
 def apply(data):
     dirpath = tempfile.mkdtemp(dir=os.getcwd())
-    ff = open(dirpath + '/temp.yaml', 'w+')
+    ff = open(dirpath + "/temp.yaml", "w+")
     yaml.dump(data, ff, allow_unicode=True, sort_keys=False)
     ff.close()
     os.system("kubectl apply -f " + dirpath)
     os.system("rm -r " + dirpath)
 
 
-
-
-
-
+def apply(data, dirpath):
+    kind = data["kind"]
+    name = data["metadata"]["name"]
+    filename = f"{kind}_{name}.yaml".lower().replace(" ", "_")
+    filepath = os.path.join(dirpath, filename)
+    with open(filepath, "w+") as ff:
+        yaml.dump(data, ff, allow_unicode=True, sort_keys=False)
+    os.system(f"kubectl apply -f {filepath}")
