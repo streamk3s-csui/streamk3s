619897f6913bb38d556883e80fc4cb470f8f8bfe
eddoog <codewithalfredo@gmail.com>
2025-04-19 21:34:02 +0700

    use 2 scaled objects instead of 1

--- converter_streams/KEDA.py
+++ converter_streams/KEDA.py
@@ -1,5 +1,6 @@
 import re
 import Kubernetes
+import oyaml as yaml
 import os
 import logging
 import base64
@@ -11,105 +12,91 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 def write_rules_config(operatorlist, dirpath):
+    values = [operator.get_application() for operator in operatorlist]
+    # Check if all values are the same
+    if all(v == values[0] for v in values):
+        namespace = values[0]
+        configure_rabbitmq_connection(namespace, dirpath)
+    else:
+        logging.info("Namespace cannot be different between the operators")
     for operator in operatorlist:
         deployment = operator.get_name()
-        namespace = operator.get_application()
         rule_list = operator.get_scale()
         if rule_list is not None:
-            scale_object = {
-                "apiVersion": "keda.sh/v1alpha1",
-                "kind": "ScaledObject",
-                "metadata": {"name": f"{deployment}-scale", "namespace": namespace},
-                "spec": {
-                    "scaleTargetRef": {"name": deployment},
-                    "pollingInterval": 5,
-                    "cooldownPeriod": 10,
-                    "minReplicaCount": 1,
-                    "maxReplicaCount": 1,
-                    "triggers": [],
-                },
-            }
-            max_replicas = 1
-
             for rule in rule_list:
+                rule["deployment"] = deployment
+                logging.info(rule)
+                deployment_name = rule.get("deployment")
+                scale_name = deployment_name + "scale-rule-" + str(rule.get("rule"))
+                logging.info(scale_name)
                 condition = rule.get("condition")
                 scale_up = rule.get("scale")
-                queue = rule.get("output_queue") or rule.get("input_queue")
-                if not queue:
-                    continue
-
-                # Update maxReplicaCount
-                if scale_up and scale_up > max_replicas:
-                    max_replicas = scale_up
-
-                value = int(re.search(r"\d+", condition).group())
-
+                if rule.get("output_queue"):
+                    queue = rule.get("output_queue")
+                if rule.get("input_queue"):
+                    queue = rule.get("input_queue")
                 if "QueueLength" in condition:
-                    scale_object["spec"]["triggers"].append(
-                        {
-                            "type": "rabbitmq",
-                            "metadata": {
-                                "protocol": "http",
-                                "queueName": queue,
-                                "mode": "QueueLength",
-                                "value": str(value),
-                            },
-                            "authenticationRef": {
-                                "name": "keda-trigger-auth-rabbitmq-conn"
-                            },
-                        }
-                    )
-
+                    condition_name = "QueueLength"
                 if "MessageRate" in condition:
-                    scale_object["spec"]["triggers"].append(
-                        {
-                            "type": "rabbitmq",
-                            "metadata": {
-                                "protocol": "http",
-                                "queueName": queue,
-                                "mode": "MessageRate",
-                                "value": str(value),
-                            },
-                            "authenticationRef": {
-                                "name": "keda-trigger-auth-rabbitmq-conn"
-                            },
-                        }
-                    )
+                    condition_name = "MessageRate"
+                value = int(re.search(r"\d+", condition).group())
+                scale_object = {
+                    "apiVersion": "keda.sh/v1alpha1",
+                    "kind": "ScaledObject",
+                    "metadata": {"name": scale_name, "namespace": namespace},
+                    "spec": {
+                        "scaleTargetRef": {"name": deployment_name},
+                        "pollingInterval": 5,
+                        "cooldownPeriod": 10,
+                        "minReplicaCount": 1,
+                        "maxReplicaCount": scale_up,
+                        "triggers": [
+                            {
+                                "type": "rabbitmq",
+                                "metadata": {
+                                    "protocol": "http",
+                                    "queueName": queue,
+                                    "mode": condition_name,
+                                    "value": str(value),
+                                },
+                                "authenticationRef": {
+                                    "name": "keda-trigger-auth-rabbitmq-conn"
+                                },
+                            }
+                        ],
+                    },
+                }
+                logging.info(scale_object)
+                logging.info(f"Writing {scale_name} to {dirpath}")
+                Kubernetes.apply(scale_object, dirpath)
 
-            # Set the maxReplicaCount to the highest value found
-            scale_object["spec"]["maxReplicaCount"] = max_replicas
 
-            logging.info(scale_object)
-            Kubernetes.apply(scale_object, dirpath)
+def configure_rabbitmq_connection(namespace, dirpath):
 
-        password = os.getenv("RABBITMQ_PASSWORD", "password")
-        ip = os.getenv("POD_IP", "ip")
-        username = "user"
-        uri = f"http://{username}:{password}@{ip}:15672/{namespace}"
+    password = os.getenv("RABBITMQ_PASSWORD", "password")
+    ip = os.getenv("POD_IP", "ip")
+    username = "user"
+    uri = "http://" + username + ":" + password + "@" + ip + ":15672/" + namespace
 
-        uri_bytes = uri.encode("ascii")
-        base64_uri = base64.b64encode(uri_bytes)
-        credentials = base64_uri.decode("ascii")
+    uri_bytes = uri.encode("ascii")
+    base64_uri = base64.b64encode(uri_bytes)
+    credentials = base64_uri.decode("ascii")
 
-        secret_config = {
-            "apiVersion": "v1",
-            "kind": "Secret",
-            "metadata": {"name": "keda-rabbitmq-secret", "namespace": namespace},
-            "data": {"host": credentials},
-        }
-        Kubernetes.apply(secret_config, dirpath)
-
-        trigger_auth = {
-            "apiVersion": "keda.sh/v1alpha1",
-            "kind": "TriggerAuthentication",
-            "metadata": {
-                "name": "keda-trigger-auth-rabbitmq-conn",
-                "namespace": namespace,
-            },
-            "spec": {
-                "secretTargetRef": [
-                    {"parameter": "host", "name": "keda-rabbitmq-secret", "key": "host"}
-                ]
-            },
-        }
-        Kubernetes.apply(trigger_auth, dirpath)
+    secret_config = {
+        "apiVersion": "v1",
+        "kind": "Secret",
+        "metadata": {"name": "keda-rabbitmq-secret", "namespace": namespace},
+        "data": {"host": credentials},
+    }
+    Kubernetes.apply(secret_config, dirpath)
+    trigger_auth = {
+        "apiVersion": "keda.sh/v1alpha1",
+        "kind": "TriggerAuthentication",
+        "metadata": {"name": "keda-trigger-auth-rabbitmq-conn", "namespace": namespace},
+        "spec": {
+            "secretTargetRef": [
+                {"parameter": "host", "name": "keda-rabbitmq-secret", "key": "host"}
+            ]
+        },
+    }
+    Kubernetes.apply(trigger_auth)
